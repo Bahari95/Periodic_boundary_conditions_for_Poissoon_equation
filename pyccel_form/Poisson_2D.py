cff2b7c0ba1b5a92c7f711a666eee88c0a304ddf
Bahari95 <75118127+Bahari95@users.noreply.github.com>
2022-04-11 15:13:18 +0000

    Add files via upload

---
+++ pyccel_form/Poisson_2D.py
@@ -0,0 +1,151 @@
+from simplines import compile_kernel, apply_dirichlet
+
+from simplines import SplineSpace
+from simplines import TensorSpace
+from simplines import StencilMatrix
+from simplines import StencilVector
+from simplines import sol_field_2d
+from simplines import plot_field_2d
+
+#.. Prologation by knots insertion matrix
+from simplines import prolongation_matrix
+
+
+#from matplotlib.pyplot import plot, show
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+#%matplotlib inline
+import timeit
+import time
+start = time.time()
+
+from gallery_section_00 import assemble_stiffnessmatrix1D
+from gallery_section_00 import assemble_massmatrix1D
+
+assemble_stiffness1D = compile_kernel( assemble_stiffnessmatrix1D, arity=2)
+assemble_mass1D      = compile_kernel( assemble_massmatrix1D, arity=2)
+
+#---In Poisson equation
+from gallery_section_00 import assemble_vector_ex01 #---1 : In uniform mesh
+from gallery_section_00 import assemble_norm_ex01 #---1 : In uniform mesh
+
+assemble_rhs         = compile_kernel(assemble_vector_ex01, arity=1)
+assemble_norm_l2  = compile_kernel(assemble_norm_ex01, arity=1)
+
+print('time to import utilities of Poisson equation =', time.time()-start)
+
+
+from scipy.sparse import kron
+from scipy.sparse import csr_matrix
+from kronecker.kronecker import vec_2d
+from kronecker.fast_diag import Poisson
+from scipy.sparse import csc_matrix, linalg as sla
+from numpy import zeros, linalg, asarray
+import numpy as np
+
+from tabulate import tabulate
+
+#==============================================================================
+#.......Poisson ALGORITHM
+def poisson_solve(V1, V2 , V):
+
+       u   = StencilVector(V.vector_space)
+       #... We delete the first and the last spline function
+       #. as a technic for applying Dirichlet boundary condition
+       
+       #..Stiffness and Mass matrix in 1D in the first deriction
+       K1 = assemble_stiffness1D(V1)
+       K1 = K1.tosparse()
+       K1 = K1.toarray()
+       K1[0,:]  = 0.
+       K1[-1,:] = 0.
+       #.
+       K1[0,0]   = 1.
+       K1[-1,-1] = 1.
+       K1 = csr_matrix(K1)
+       
+       M1 = assemble_mass1D(V1)
+       M1 = M1.tosparse()
+       M1 = M1.toarray()
+       M1[0,:]  = 0.
+       M1[-1,:] = 0.
+       #.
+       M1[0,0]   = 1.
+       M1[-1,-1] = 1.
+       M1 = csr_matrix(M1)
+
+       # Stiffness and Mass matrix in 1D in the second deriction
+       K2 = assemble_stiffness1D(V2)
+       K2 = K2.tosparse()
+       K2 = K2.toarray()
+       K2 = csr_matrix(K2)
+
+       M2 = assemble_mass1D(V2)
+       M2 = M2.tosparse()
+       M2 = M2.toarray()
+       M2 = csr_matrix(M2)
+
+       mats_1 = [M1, K1]
+       mats_2 = [M2, K2]
+
+       # ...
+       poisson = Poisson(mats_1, mats_2)
+      
+       #---- assemble rhs
+       rhs = assemble_rhs( V )
+       rhs[0,:]           = 0.
+       rhs[V1.nbasis-1,:] = 0.
+       #--Solve a linear system
+
+       b  = rhs.toarray()
+
+       #x  = lu.solve(b)
+       #x, inf   = sla.cg(M, b)
+       x  = poisson.solve(b)
+       x        = x.reshape(V.nbasis)
+       x[0, :]  = 25000.
+       u.from_array(V, x)
+
+       Norm    = assemble_norm_l2(V, fields=[u])
+       norm    = Norm.toarray()
+       l2_norm = norm[0]
+       H1_norm = norm[1]
+       
+       return u, x, l2_norm, H1_norm
+
+degree     = 3
+nelements  = 64
+
+#----------------------
+# create the spline space for each direction
+V1 = SplineSpace(degree=degree, nelements= nelements, nderiv = 2)
+V2 = SplineSpace(degree=degree, nelements= nelements, nderiv = 2,periodic=True)
+
+# create the tensor space
+Vh = TensorSpace(V1, V2)
+
+print('#---IN-UNIFORM--MESH')
+u_ph, xuh, l2_norm, H1_norm = poisson_solve(V1, V2, Vh)
+print('l2_norm = {} H1_norm = {} '.format(l2_norm, H1_norm) ) 
+nbpts    = 50
+
+u_poisson, a, b, Y, X = sol_field_2d((nbpts,nbpts),  xuh , Vh.knots, Vh.degree)
+figtitle  = 'solution of poisson equation in the new geometry'
+
+fig, axes = plt.subplots( 1, 2, figsize=[12,12], gridspec_kw={'width_ratios': [2.75, 2]} , num=figtitle )
+for ax in axes:
+   ax.set_aspect('equal')
+
+#axes[0].set_title( 'electric potential Function' )
+im = axes[0].contourf( X,Y, u_poisson, cmap= 'jet')
+fig.colorbar(im, shrink=0.52, aspect=20)
+#axes[1].set_title( 'exact electric potential Function' )
+im = axes[1].contourf( X,Y, u_poisson, cmap= 'jet')
+fig.colorbar(im, shrink=0.52, aspect=20)
+fig.tight_layout()
+plt.subplots_adjust(wspace=0.3)
+plt.savefig('periodic_Poisson')
+plt.show()
---
+++ pyccel_form/gallery_section_00.py
@@ -0,0 +1,199 @@
+__all__ = ['assemble_stiffnessmatrix1D',
+           'assemble_massmatrix1D',
+           'assemble_norm_ex01',
+]
+
+
+#==============================================================================Assembles stiffness matrix
+#---1 : In uniform mesh
+from pyccel.decorators import types
+# assembles stiffness matrix 1D
+#==============================================================================
+@types('int', 'int', 'int[:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]')
+def assemble_stiffnessmatrix1D(ne, degree, spans, basis, weights, points, matrix):
+
+    # ... sizes
+    k1 = weights.shape[1]
+    # ... build matrices
+    for ie1 in range(0, ne):
+            i_span_1 = spans[ie1]        
+            # evaluation dependant uniquement de l'element
+
+            for il_1 in range(0, degree+1):
+                i1 = i_span_1 - degree + il_1
+                for il_2 in range(0, degree+1):
+                            i2 = i_span_1 - degree + il_2
+                            v = 0.0
+                            for g1 in range(0, k1):
+                                
+                                    bi_x = basis[ie1, il_1, 1, g1]
+                                    bj_x = basis[ie1, il_2, 1, g1]
+                                    
+                                    wvol = weights[ie1, g1]
+                                    
+                                    v += bi_x * bj_x * wvol
+
+                            matrix[ degree+ i1, degree+ i2-i1]  += v
+
+# assembles mass matrix 1D
+#==============================================================================
+@types('int', 'int', 'int[:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]')
+def assemble_massmatrix1D(ne, degree, spans, basis, weights, points,  matrix):
+
+    # ... sizes
+    k1 = weights.shape[1]
+    # ... build matrices
+    for ie1 in range(0, ne):
+            i_span_1 = spans[ie1]        
+            # evaluation dependant uniquement de l'element
+
+            for il_1 in range(0, degree+1):
+                i1 = i_span_1 - degree + il_1
+                for il_2 in range(0, degree+1):
+                            i2 = i_span_1 - degree + il_2
+                            v = 0.0
+                            for g1 in range(0, k1):
+                                
+                                    bi_0 = basis[ie1, il_1, 0, g1]
+                                    bj_0 = basis[ie1, il_2, 0, g1]
+                                    
+                                    wvol = weights[ie1, g1]
+                                    
+                                    v += bi_0 * bj_0 * wvol
+
+                            matrix[degree+i1, degree+ i2-i1]  += v
+
+
+#==============================================================================Assemble rhs Poisson
+#---1 : In uniform mesh
+@types('int', 'int', 'int', 'int', 'int[:]', 'int[:]', 'double[:,:,:,:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]',  'double[:,:]')
+def assemble_vector_ex01(ne1, ne2, p1, p2, spans_1, spans_2,  basis_1, basis_2,  weights_1, weights_2, points_1, points_2, rhs):
+
+    from numpy import exp
+    from numpy import cos
+    from numpy import sin
+    from numpy import pi
+    from numpy import sqrt
+    from numpy import zeros
+
+    # ... sizes
+    k1 = weights_1.shape[1]
+    k2 = weights_2.shape[1]
+    # ...
+
+    lvalues_u = zeros((k1, k2))
+
+    int_rhsP = 0.0
+    # ... build rhs
+    for ie1 in range(0, ne1):
+        i_span_1 = spans_1[ie1]
+        for ie2 in range(0, ne2):
+            i_span_2 = spans_2[ie2]
+
+            lvalues_u[ : , : ] = 0.0
+            for g1 in range(0, k1):
+                for g2 in range(0, k2):
+
+                    x    = points_1[ie1, g1]
+                    y    = points_2[ie2, g2]
+
+                    # ... test 1     
+                    f = 0. #2.*pi**2 * sin(pi*x)*sin(pi*y)
+
+                    lvalues_u[g1,g2] = f
+            for il_1 in range(0, p1+1):
+                for il_2 in range(0, p2+1):
+                    i1 = i_span_1 - p1 + il_1
+                    i2 = i_span_2 - p2 + il_2
+
+                    v = 0.0
+                    for g1 in range(0, k1):
+                        for g2 in range(0, k2):
+                            bi_0 = basis_1[ie1, il_1, 0, g1] * basis_2[ie2, il_2, 0, g2]
+
+                            wvol  = weights_1[ie1, g1]*weights_2[ie2, g2]
+
+                            u  = lvalues_u[g1,g2]
+                            v += bi_0 * u * wvol
+
+                    rhs[i1+p1,i2+p2] += v   
+    # ...
+
+#==============================================================================Assemble l2 and H1 error norm
+#---1 : In uniform mesh
+@types('int', 'int', 'int', 'int', 'int[:]', 'int[:]', 'double[:,:,:,:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]',  'double[:,:]','double[:,:]')
+def assemble_norm_ex01(ne1, ne2, p1, p2, spans_1, spans_2,  basis_1, basis_2,  weights_1, weights_2, points_1, points_2, vector_u, rhs):
+
+    from numpy import exp
+    from numpy import cos
+    from numpy import sin
+    from numpy import pi
+    from numpy import sqrt
+    from numpy import zeros
+
+    # ... sizes
+    k1 = weights_1.shape[1]
+    k2 = weights_2.shape[1]
+    # ...
+
+    lcoeffs_u = zeros((p1+1,p2+1))
+    lvalues_u = zeros((k1, k2))
+    lvalues_ux = zeros((k1, k2))
+    lvalues_uy = zeros((k1, k2))
+
+    norm_l2 = 0.
+    norm_H1 = 0.
+    # ...
+    for ie1 in range(0, ne1):
+        i_span_1 = spans_1[ie1]
+        for ie2 in range(0, ne2):
+            i_span_2 = spans_2[ie2]
+
+            lvalues_u[ : , : ]  = 0.0
+            lvalues_ux[ : , : ] = 0.0
+            lvalues_uy[ : , : ] = 0.0
+            lcoeffs_u[ : , : ]  = vector_u[i_span_1 : i_span_1+p1+1, i_span_2 : i_span_2+p2+1]
+            for il_1 in range(0, p1+1):
+                for il_2 in range(0, p2+1):
+                    coeff_u = lcoeffs_u[il_1,il_2]
+
+                    for g1 in range(0, k1):
+                        b1  = basis_1[ie1,il_1,0,g1]
+                        db1 = basis_1[ie1,il_1,1,g1]
+                        for g2 in range(0, k2):
+                            b2  = basis_2[ie2,il_2,0,g2]
+                            db2 = basis_2[ie2,il_2,1,g2]
+
+                            lvalues_u[g1,g2]  += coeff_u*b1*b2
+                            lvalues_ux[g1,g2] += coeff_u*db1*b2
+                            lvalues_uy[g1,g2] += coeff_u*b1*db2
+
+            v = 0.0
+            w = 0.0
+            for g1 in range(0, k1):
+                for g2 in range(0, k2):
+                    wvol = weights_1[ie1, g1] * weights_2[ie2, g2]
+
+                    x    = points_1[ie1, g1]
+                    y    = points_2[ie2, g2]
+ 
+                    # ...
+                    u  = sin(pi*x)*sin(pi*y)
+                    ux = pi*cos(pi*x)*sin(pi*y)
+                    uy = pi*sin(pi*x)*cos(pi*y)
+                    # ...
+                    uh  = lvalues_u[g1,g2]
+                    uhx = lvalues_ux[g1,g2]
+                    uhy = lvalues_uy[g1,g2]
+
+                    v  += (u-uh)**2 * wvol
+                    w  += ((ux-uhx)**2+(uy-uhy)**2) * wvol
+            norm_l2 += v
+            norm_H1 += w
+
+    norm_l2 = sqrt(norm_l2)
+    norm_H1 = sqrt(norm_H1)
+
+    rhs[p1,p2] = norm_l2
+    rhs[p1,p2+1] = norm_H1
+    # ...

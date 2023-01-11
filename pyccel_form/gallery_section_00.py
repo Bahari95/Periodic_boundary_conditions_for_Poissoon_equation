eca8ede07f84811aba6382df9a8ec035e110c126
Bahari Mustapha <75118127+Bahari95@users.noreply.github.com>
2023-01-11 13:41:32 +0100

    Add files via upload

--- pyccel_form/gallery_section_00.py
+++ pyccel_form/gallery_section_00.py
@@ -64,7 +64,50 @@ def assemble_massmatrix1D(ne, degree, spans, basis, weights, points,  matrix):
 
                             matrix[degree+i1, degree+ i2-i1]  += v
 
+#==============================================================================Assembles stiffness matrix
+#---1 : In uniform mesh
+from pyccel.decorators import types
+@types('int', 'int', 'int', 'int', 'int[:]', 'int[:]', 'double[:,:,:,:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:,:,:]')
+def assemble_matrix_2D_ex01(ne1, ne2, p1, p2,  spans_1, spans_2, basis_1, basis_2, weights_1, weights_2, points_1, points_2, matrix):
 
+    # ... sizes
+    k1 = weights_1.shape[1]
+    k2 = weights_2.shape[1]
+    # ...
+
+    # ... build matrices
+    for ie1 in range(0, ne1):
+        i_span_1 = spans_1[ie1]
+        for ie2 in range(0, ne2):
+            i_span_2 = spans_2[ie2]
+
+            for il_1 in range(0, p1+1):
+                for il_2 in range(0, p2+1):
+                    for jl_1 in range(0, p1+1):
+                        for jl_2 in range(0, p2+1):
+                            i1 = i_span_1 - p1 + il_1
+                            j1 = i_span_1 - p1 + jl_1
+
+                            i2 = i_span_2 - p2 + il_2
+                            j2 = i_span_2 - p2 + jl_2
+
+                            v = 0.0
+                            for g1 in range(0, k1):
+                                for g2 in range(0, k2):
+                                    bi_x = basis_1[ie1, il_1, 1, g1] * basis_2[ie2, il_2, 0, g2]
+                                    bi_y = basis_1[ie1, il_1, 0, g1] * basis_2[ie2, il_2, 1, g2]
+
+
+                                    bj_x = basis_1[ie1, jl_1, 1, g1] * basis_2[ie2, jl_2, 0, g2]
+                                    bj_y = basis_1[ie1, jl_1, 0, g1] * basis_2[ie2, jl_2, 1, g2]
+
+                                    wvol = weights_1[ie1, g1] * weights_2[ie2, g2]
+
+                                    v += (bi_x * bj_x + bi_y * bj_y) * wvol
+
+                            matrix[p1+i1, p2+i2, p1+j1-i1, p2+j2-i2]  += v
+    # ...
+    
 #==============================================================================Assemble rhs Poisson
 #---1 : In uniform mesh
 @types('int', 'int', 'int', 'int', 'int[:]', 'int[:]', 'double[:,:,:,:]', 'double[:,:,:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]', 'double[:,:]')
